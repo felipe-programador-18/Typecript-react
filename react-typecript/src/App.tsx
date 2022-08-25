66241dffcb6ac6a97ffc2175901c2e624b801455
Felipe <machadofelipe2016@outlook.com>
2022-08-25 13:40:36 -0300

    c4-creating another function to remerber more about this code

--- react-typecript/src/App.tsx
+++ react-typecript/src/App.tsx
@@ -17,19 +17,37 @@ const Greetings = (a:String): String => {
    return `name is ${a}`
 }
 
+const phase:string ='I am fullstack developer react node typescript and who know in the future i study about ruby on ralls  '
+
+const Presentation = (phase:string): string =>{
+  return `i need training more about typescript with graphql ${phase}`
+}
+
+let v: number =323
+let t:number =323
+
+const Increased = (t:number, v:number):number => {
+  return t*v
+}
+
+
   return (
     <div className="App">
      <h1>Typescript with react!!!
      Nome: {a} 
     </h1>
       
+     <p>My sum is !! {Increased(t,v)} </p>
      <p>{Greetings(a)}</p> 
+      
+     <h1> {Presentation(phase)} </h1>
+
 
     <AnotherComponent/>
     {working && <p> trabalhando</p> }
     <Firstcomponent name='felipe programmer 18'  />
 
-    <TraninDesctruc  title='adding practice quite about typescript and react!!' age={2334234}  tags={['receive here more issues to need practice more and more always', 'on the other hand',
+    <TraninDesctruc commets='today I want traning little bit about graphql and typescript'  title='adding practice quite about typescript and react!!' age={2334234}  tags={['receive here more issues to need practice more and more always', 'on the other hand',
   'programmer Felipe Progammer Felipe']} />
     
     </div>
