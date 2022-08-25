a906a747d8c40a9e92c37856f35c963786883644
Felipe <machadofelipe2016@outlook.com>
2022-08-25 13:39:35 -0300

    c3-traning more little bit about destructing props

--- react-typecript/src/Component/destructing.tsx
+++ react-typecript/src/Component/destructing.tsx
@@ -4,15 +4,15 @@ export interface AnotherInter{
     title: String
     age: Number
     tags : String[]
+    commets: String
 }
 
-export function TraninDesctruc ({title, age,tags}:AnotherInter) {
+export function TraninDesctruc ({title, age,tags, commets}:AnotherInter) {
   return (<> 
     Now i need destructing my props {`This my title ${title} and my ${age}`}
-    <div>
-        
-        {tags.map((tag) => (<p> {tag} </p>))}
-
+    <div> 
+      {tags.map((tag) => (<p> {tag} </p>))}
+      {commets}
     </div>
 
  
