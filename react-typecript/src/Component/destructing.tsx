ef36b2fbc346c9feb792efa76944d45adb8ecf91
Felipe <machadofelipe2016@outlook.com>
2022-08-28 16:18:19 -0300

    c12-practice little bit

--- react-typecript/src/Component/destructing.tsx
+++ react-typecript/src/Component/destructing.tsx
@@ -5,14 +5,25 @@ export interface AnotherInter{
     age: Number
     tags : String[]
     commets: String
+    
+    category: Category
+    // eight enum
 }
 
-export function TraninDesctruc ({title, age,tags, commets}:AnotherInter) {
+export enum Category {
+  JS = 'adding enum here interest functionality',
+  TS ='Typescript is very good',
+  P ="python"
+}
+
+export function TraninDesctruc ({title, age,tags, commets, category}:AnotherInter) {
   return (<> 
     Now i need destructing my props {`This my title ${title} and my ${age}`}
     <div> 
       {tags.map((tag) => (<p> {tag} </p>))}
       {commets}
+
+      <h1>Categoria :  {category} </h1>
     </div>
 
  
