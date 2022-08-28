4b290f9c10ca702be4a4bde35692090194965110
Felipe <machadofelipe2016@outlook.com>
2022-08-28 16:18:36 -0300

    c13-adding more code htere

--- react-typecript/src/App.tsx
+++ react-typecript/src/App.tsx
@@ -4,8 +4,18 @@ import { AnotherComponent } from './Component/anothercomponent';
 import './App.css';
 
 import { DoMore } from './Component/traningmore';
-import { TraninDesctruc }  from './Component/destructing'
+import { TraninDesctruc, Category }  from './Component/destructing'
 
+import StateReact from './Component/state.type';
+import AnotherState from './Component/interesting.state';
+import CreateMore from './Component/addingmoreprops';
+import AnotherApp from './Component/Statemore';
+
+
+
+
+//creating another code about type
+type AddingTest =  string | null
 
 
 function App() {
@@ -30,13 +40,25 @@ const Increased = (t:number, v:number):number => {
   return t*v
 }
 
+// type with typescript
+
+//type
+
+const receive: AddingTest = 'adding testing here';
+let receiveanother:AddingTest = null;
+receiveanother ='adding another text here practice my freinds!!'
 
   return (
     <div className="App">
+    {receive && <p>Adding more testing here {receiveanother}</p>}
+
+     <CreateMore age={232323}  name='Programmer Felipe Floripa' children={[12]}  />
+
+     <AnotherApp/>
      <h1>Typescript with react!!!
      Nome: {a} 
     </h1>
-      
+    <AnotherState/>
      <p>My sum is !! {Increased(t,v)} </p>
      <p>{Greetings(a)}</p> 
       
@@ -48,12 +70,18 @@ const Increased = (t:number, v:number):number => {
     <Firstcomponent name='felipe programmer 18'  />
 
     <TraninDesctruc commets='today I want traning little bit about graphql and typescript'  title='adding practice quite about typescript and react!!' age={2334234}  tags={['receive here more issues to need practice more and more always', 'on the other hand',
-  'programmer Felipe Progammer Felipe']} />
+    'programmer Felipe Progammer Felipe']} 
+    category={Category.JS}
+
+
+  />
 
     <DoMore name='Felipe Programmer' age={23} salary={20000} 
     gender='M' email='programmerfloripa@outlook.com'
     />
-
+   
+  
+   <StateReact/>
 
     </div>
   );
