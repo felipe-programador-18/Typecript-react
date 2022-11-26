26bde365ea514e9585af8645f0cb7d47939db773
Felipe <machadofelipe2016@outlook.com>
2022-11-26 16:58:15 -0300

    c17-create a new form and practice about usestate and form

--- react-typecript/src/Component/practice.tsx
+++ react-typecript/src/Component/practice.tsx
@@ -1,4 +1,4 @@
-import * as React from 'react'
+import  React,{useState, ChangeEvent} from 'react'
 
 export interface Dates{
   name:string,
@@ -7,8 +7,20 @@ export interface Dates{
 }
 
 export const  PracticeLittle = ({name,email,age}:Dates) => {
-
+     
+    const [carrer, setCarrer] = useState<string|null>(null)
+     
+    const Handlechange = (e:ChangeEvent<HTMLInputElement>) => {
+        setCarrer(e.target.value)
+    }
     return (<>
      <h1>Hello my name is {name} I {age} my email is: {email} </h1>
+      
+      {carrer}
+     <input 
+     type="text" 
+     placeholder='adding your name' 
+     onChange={Handlechange}
+     />
     </>)
 }
