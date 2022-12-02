e969478f0a6017cc582230bd5e519ce2eecbb887
Felipe <machadofelipe2016@outlook.com>
2022-12-02 13:13:31 -0300

    c19-create to other code to practice mroe

--- react-typecript/src/Component/morePractice.tsx
+++ react-typecript/src/Component/morePractice.tsx
@@ -1,17 +1,21 @@
 import  React from 'react';
 
-export interface IAppProps {
+export interface MyDates{
 name: string
 age: number,
-gender?: string
+gender?:"F"|"M"
 email: string
 }
 
-export function App ({name, age, gender, email}:IAppProps) {
+export function Cadastre ({name, age, gender, email}: MyDates) {
   return (
     <div>
-      
-      
+      <h2>Programmer{name}</h2>
+      <h5>my gender is{gender} and my age is {age}</h5>
+      <h4>If you want to create a new webpage you can acess here
+        {email}
+      </h4>
+
     </div>
   );
 }
