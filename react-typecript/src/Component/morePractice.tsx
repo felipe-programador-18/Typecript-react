a0901384f16663238e951e20d5fa43e9cdbd3cb6
Felipe <machadofelipe2016@outlook.com>
2022-12-02 13:09:39 -0300

    c18-create to other code to practice mroe

---
+++ react-typecript/src/Component/morePractice.tsx
@@ -0,0 +1,17 @@
+import  React from 'react';
+
+export interface IAppProps {
+name: string
+age: number,
+gender?: string
+email: string
+}
+
+export function App ({name, age, gender, email}:IAppProps) {
+  return (
+    <div>
+      
+      
+    </div>
+  );
+}
